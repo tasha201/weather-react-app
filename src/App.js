b4682010196d2c787ababf90675f8ac9ed7d5d8e
tasha201 <natalia.it.designer@gmail.com>
2023-01-05 23:22:29 +0200

    Adding links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather city="Kyiv" />
+        <p className="link">
+          This Weather React App was coded by Natalia Pavlyk and is{" "}
+          <a
+            href="https://github.com/tasha201/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced
+          </a>{" "}
+          and <a href="https://fanciful-kringle-a1a815.netlify.app" target="_blank"
+            rel="noreferrer">hosted</a> on Netlify
+        </p>
       </header>
     </div>
   );
