1fa08ad673e789970df4884331d866a9f90c4ded
tasha201 <natalia.it.designer@gmail.com>
2023-01-09 23:46:55 +0200

    Adding some changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-        {/* <p className="link">
+      <h1>Weather App</h1>
+        <footer>
           This Weather React App was coded by Natalia Pavlyk and is{" "}
           <a
             href="https://github.com/tasha201/weather-react-app"
@@ -13,11 +13,11 @@ export default function App() {
             rel="noreferrer"
           >
             {" "}
-            open-sourced
+            open-sourced on Github
           </a>{" "}
           and <a href="https://fanciful-kringle-a1a815.netlify.app" target="_blank"
             rel="noreferrer">hosted</a> on Netlify
-        </p> */}
+        </footer>
       
     </div>
   );
